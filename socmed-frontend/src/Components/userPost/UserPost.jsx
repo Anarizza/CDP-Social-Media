c66ef0a4a09ee352c340201642aaf0b8b703642d
Nicolas <78528181+NicolasLibre@users.noreply.github.com>
2023-03-02 09:29:46 +0800

    change color delete button in delete post sweetalert

--- socmed-frontend/src/Components/userPost/UserPost.jsx
+++ socmed-frontend/src/Components/userPost/UserPost.jsx
@@ -113,7 +113,7 @@ const UserPost = ({ post }) => {
         text: "You won't be able to revert this!",
         icon: "warning",
         showCancelButton: true,
-        confirmButtonColor: "#3085d6",
+        confirmButtonColor: "#00796b",
         cancelButtonColor: "#d33",
         confirmButtonText: "Yes, delete it!",
       }).then((result) => {
